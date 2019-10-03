a2ed13cd1737afaf1093b4a722cc85c82735ec83
William Sedlacek <wsedlacekc@gmail.com>
2019-10-03 15:38:18 -0500

    Setup Hook
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/app/hooks/score-board/score-board.hook.ts
+++ src/app/hooks/score-board/score-board.hook.ts
@@ -1,20 +1,51 @@
 import { useState } from 'react';
-import { ScoreBoard } from '../../models/ScoreBoard';
+import { ScoreBoard, ScoreBoardActions } from '../../models/ScoreBoard';
 
-const useScoreBoard = (initialValue?: ScoreBoard): [ScoreBoard, (value: ScoreBoard) => void] => {
-  if (!initialValue) initialValue = {};
+const useScoreBoard = (initialValue?: ScoreBoard): [ScoreBoard, ScoreBoardActions] => {
+  if (!initialValue) initialValue = { balls: 0, outs: 0, strikes: 0 };
 
   const [balls, setBalls] = useState(initialValue.balls || 0);
   const [strikes, setStrikes] = useState(initialValue.strikes || 0);
   const [outs, setOuts] = useState(initialValue.outs || 0);
 
-  const setScoreBoard = ({ balls, strikes, outs }: ScoreBoard) => {
-    if (balls) setBalls(balls);
-    if (strikes) setStrikes(strikes);
-    if (outs) setOuts(outs);
+  const out = () => {
+    setOuts(outs + 1);
+    if (outs >= 3) setOuts(0);
   };
 
-  return [{ balls, strikes, outs }, setScoreBoard];
+  const ball = () => {
+    const newVal = balls + 1;
+    if (newVal >= 4) {
+      setStrikes(0);
+      setBalls(0);
+    } else {
+      setBalls(newVal);
+    }
+  };
+
+  const strike = () => {
+    const newVal = strikes + 1;
+    if (newVal >= 3) {
+      setStrikes(0);
+      setBalls(0);
+      out();
+    } else {
+      setStrikes(newVal);
+    }
+  };
+
+  const foul = () => {
+    if (strikes < 2) {
+      strike();
+    }
+  };
+
+  const hit = () => {
+    setStrikes(0);
+    setBalls(0);
+  };
+
+  return [{ balls, strikes, outs }, { out, ball, strike, foul, hit }];
 };
 
 export { useScoreBoard };
