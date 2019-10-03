c2638f9db488f79db387673b3d503f4d5bb5a561
William Sedlacek <wsedlacekc@gmail.com>
2019-10-03 15:38:32 -0500

    Setup Display
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/app/components/display/display.component.tsx
+++ src/app/components/display/display.component.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const Display = () => {
-  return <div>Test</div>;
+import { ScoreBoard } from '../../models/ScoreBoard';
+
+const Display = ({ balls, strikes, outs }: ScoreBoard) => {
+  return (
+    <div>
+      <div>Balls: {balls}</div>
+      <div>Strikes: {strikes}</div>
+      <div>Outs: {outs}</div>
+    </div>
+  );
 };
 
 export { Display };
