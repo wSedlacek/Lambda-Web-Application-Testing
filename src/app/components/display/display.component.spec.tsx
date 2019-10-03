bcc2a6b794176caf9971baa5bceab65168b9ef64
William Sedlacek <wsedlacekc@gmail.com>
2019-10-03 15:38:26 -0500

    Setup Display Test
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/app/components/display/display.component.spec.tsx
+++ src/app/components/display/display.component.spec.tsx
@@ -1,12 +1,32 @@
-// - display the count of `balls` and `strikes` for the at-bat.
-// - should be updated when the user records activity on the `Dashboard` component.
-
 import React from 'react';
 import { render } from '@testing-library/react';
 
 import { Display } from './display.component';
 
+const TestBed = () => <Display balls={3} strikes={2} outs={1} />;
+
 test('renders without errors', () => {
-  const { baseElement } = render(<Display />);
+  const { baseElement } = render(<TestBed />);
   expect(baseElement).toBeTruthy();
+});
+
+test('show balls based on prop', () => {
+  const { getByText } = render(<TestBed />);
+  const balls = getByText(/balls/i);
+  expect(balls).toBeTruthy();
+  expect(balls.innerHTML.includes('3')).toBeTruthy();
+});
+
+test('show strikes based on prop', () => {
+  const { getByText } = render(<TestBed />);
+  const strikes = getByText(/strikes/i);
+  expect(strikes).toBeTruthy();
+  expect(strikes.innerHTML.includes('2')).toBeTruthy();
+});
+
+test('show outs based on prop', () => {
+  const { getByText } = render(<TestBed />);
+  const outs = getByText(/outs/i);
+  expect(outs).toBeTruthy();
+  expect(outs.innerHTML.includes('1')).toBeTruthy();
 });
