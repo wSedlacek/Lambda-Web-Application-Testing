b0e6b884d183598c852b2c6bf1d7352f219c5811
William Sedlacek <wsedlacekc@gmail.com>
2019-10-03 15:38:44 -0500

    Setup Dashboard Test
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/app/components/dashboard/dashboard.component.spec.tsx
+++ src/app/components/dashboard/dashboard.component.spec.tsx
@@ -1,13 +1,37 @@
-// - provide a button that the person in charge can press every time there is a `strike`, `ball`, `foul` or `hit`.
-// - there is **NO** need to specify the type of hit (single, double, etc).
-// - changes recorded on this component should update the information shown by the `Display` component.
-
 import React from 'react';
 import { render } from '@testing-library/react';
 
 import { Dashboard } from './dashboard.component';
 
+const stub = () => {};
+const TestBed = () => <Dashboard out={stub} strike={stub} foul={stub} ball={stub} hit={stub} />;
+
 test('renders without errors', () => {
-  const { baseElement } = render(<Dashboard />);
+  const { baseElement } = render(<TestBed />);
   expect(baseElement).toBeTruthy();
+});
+
+test('provides a out button', () => {
+  const { getByText } = render(<TestBed />);
+  expect(getByText(/out/i)).toBeTruthy();
+});
+
+test('provides a strike button', () => {
+  const { getByText } = render(<TestBed />);
+  expect(getByText(/strike/i)).toBeTruthy();
+});
+
+test('provides a foul button', () => {
+  const { getByText } = render(<TestBed />);
+  expect(getByText(/foul/i)).toBeTruthy();
+});
+
+test('provides a ball button', () => {
+  const { getByText } = render(<TestBed />);
+  expect(getByText(/ball/i)).toBeTruthy();
+});
+
+test('provides a hit button', () => {
+  const { getByText } = render(<TestBed />);
+  expect(getByText(/hit/i)).toBeTruthy();
 });
