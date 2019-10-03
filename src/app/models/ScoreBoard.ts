af7f7e919790b799d1b393b5a133644e08b9c327
William Sedlacek <wsedlacekc@gmail.com>
2019-10-03 15:37:56 -0500

    Refine Models
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/app/models/ScoreBoard.ts
+++ src/app/models/ScoreBoard.ts
@@ -1,5 +1,13 @@
 export interface ScoreBoard {
-  balls?: number;
-  strikes?: number;
-  outs?: number;
+  balls: number;
+  strikes: number;
+  outs: number;
 }
+
+export type ScoreBoardActions = {
+  out: () => void;
+  ball: () => void;
+  strike: () => void;
+  foul: () => void;
+  hit: () => void;
+};
